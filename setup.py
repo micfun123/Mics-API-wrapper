b5ace30064ae580f62a80cba31ab29e3d762467f
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-03-20 07:05:45 +0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+ 
+VERSION = '0.0.1'
+DESCRIPTION = "An API wrapper for both of the APIS I have made. "
+
+setup(
+    name='Mics_API_Wrapper',
+    version=VERSION,
+    author='Michael Parker',
+    author_email='michaelrbparker@protonmail.com',
+    url='https://github.com/micfun123/Mics-API-wrapper',
+    description=DESCRIPTION,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages()
+)
+
+# python3 setup.py bdist_wheel
+# twine upload dist/*
+# sudo rm -rf ./build ./dist ./*egg-info

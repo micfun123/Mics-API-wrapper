79a423979e4d6bbc2b85fbb82708bdabd21cc498
FusionSid <siddheshadsv@icloud.com>
2022-03-24 08:25:49 +1300

    Fixed return type from Image to Image.Image

--- MicsAPIwrapper/textmaker.py
+++ MicsAPIwrapper/textmaker.py
@@ -3,7 +3,7 @@ from io import BytesIO
 from PIL import Image
 
 
-def MctextMaker(text) -> Image:
+def MctextMaker(text) -> Image.Image:
     message = text.replace(" ", "%20")
     response = requests.get('https://michaelstextapi.herokuapp.com/api/text/Minecraft?Text={}'.format(message))
     image_data = BytesIO(response.content)
@@ -11,7 +11,7 @@ def MctextMaker(text) -> Image:
     image = Image.open(image_data)
     return(image)
 
-def Pokemontextmaker(text) -> Image:
+def Pokemontextmaker(text) -> Image.Image:
     message = text.replace(" ", "%20")
     response = requests.get("https://michaelstextapi.herokuapp.com/api/text/pokemon?Text={}" .format(message))
     image_data = BytesIO(response.content)

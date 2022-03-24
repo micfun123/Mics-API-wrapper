be10a8c47493daa500dc2501a83421d8f06c7b24
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-03-24 07:10:14 +0400

    extra info

--- MicsAPIwrapper/wallpapers.py
+++ MicsAPIwrapper/wallpapers.py
@@ -5,25 +5,25 @@ from PIL import Image
 urlwallpaperjson = 'https://micswallpaperapi.herokuapp.com/json/wallpaper'
 urlmoviewallpaperjson = 'https://micswallpaperapi.herokuapp.com/json/Film_TV_Wallpaper'
 
-def Wallpapersimagelink():
+def Wallpapersimagelink() -> dict:
     response = requests.get(urlwallpaperjson)
     response = response.json()
     return response['img_url']
 
-def filmimagelink():
+def filmimagelink() -> dict:
     response = requests.get(urlmoviewallpaperjson)
     response = response.json()
     return response['img_url']
 
 
-def wallpapersimage() -> Image:
+def wallpapersimage() -> Image.Image:
     response = requests.get('https://micswallpaperapi.herokuapp.com/wallpaper')
     image_data = BytesIO(response.content)
     image_data.seek(0)
     image = Image.open(image_data)
     return(image)
 
-def moviewallpaperimage() -> Image:
+def moviewallpaperimage() -> Image.Image:
     response = requests.get('https://micswallpaperapi.herokuapp.com/Film_TV_Wallpaper')
     image_data = BytesIO(response.content)
     image_data.seek(0)

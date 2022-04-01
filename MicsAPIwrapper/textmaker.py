115b9707c4e4821ba077d1265c9a5ea35bf8bdb6
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-04-01 21:45:59 +0400

    fix link

--- MicsAPIwrapper/textmaker.py
+++ MicsAPIwrapper/textmaker.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 def MctextMaker(text) -> Image.Image:
     message = text.replace(" ", "%20")
-    response = requests.get('https://michaelstextapi.herokuapp.com/api/text/Minecraft?Text={}'.format(message))
+    response = requests.get('https://michaelapi.herokuapp.com/api/text/Minecraft?Text={}'.format(message))
     image_data = BytesIO(response.content)
     image_data.seek(0)
     image = Image.open(image_data)
@@ -13,7 +13,15 @@ def MctextMaker(text) -> Image.Image:
 
 def Pokemontextmaker(text) -> Image.Image:
     message = text.replace(" ", "%20")
-    response = requests.get("https://michaelstextapi.herokuapp.com/api/text/pokemon?Text={}" .format(message))
+    response = requests.get("https://michaelapi.herokuapp.com/api/text/pokemon?Text={}" .format(message))
+    image_data = BytesIO(response.content)
+    image_data.seek(0)
+    image = Image.open(image_data)
+    return(image)
+
+def theavengerstextmaker(text) -> Image.Image:
+    message = text.replace(" ", "%20")
+    response = requests.get("https://michaelapi.herokuapp.com/api/text/theavengers?Text={}" .format(message))
     image_data = BytesIO(response.content)
     image_data.seek(0)
     image = Image.open(image_data)

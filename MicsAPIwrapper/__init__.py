0550c0e63431a33b6887b67efc5f3823e90ceaa7
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-04-01 21:46:30 +0400

    theavengerstextmaker

--- MicsAPIwrapper/__init__.py
+++ MicsAPIwrapper/__init__.py
@@ -2,8 +2,8 @@
 __title__ = "MicsAPIWrapper"
 __license__ = "MIT"
 __author__ = "im_the_tea_drinker"
-__version__ = "0.0.6"
+__version__ = "1.1.6"
 
 from .wallpapers import moviewallpaperimage, wallpapersimage, filmimagelink , Wallpapersimagelink
-from .textmaker import MctextMaker , Pokemontextmaker
+from .textmaker import MctextMaker , Pokemontextmaker , theavengerstextmaker
 from .drinks import teaimage, coffeeimage

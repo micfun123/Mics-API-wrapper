06de42f1a0e22e9283098697f28e84d8d9918ab1
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-04-02 16:49:29 +0400

    vertion

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
  
-VERSION = '1.1.6'
+VERSION = '2.0.0'
 DESCRIPTION = "An API wrapper for both of the APIS I have made. "
 
 setup(
@@ -18,6 +18,6 @@ setup(
     ]
 )
 
-# python3 setup.py bdist_wheel
-# twine upload dist/*
+# python3 
+# 
 # sudo rm -rf ./build ./dist ./*egg-info
